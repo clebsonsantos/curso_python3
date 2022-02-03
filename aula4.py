5e543c1840fc7f97e450e6cb89c83c63f804131f
Clebson Santos <clebson@crowde.com.br>
2022-02-03 12:53:40 -0300

    Codigo aula 04 - tipos de dados

---
+++ aula4.py
@@ -0,0 +1,27 @@
+"""
+    TIPOS DE DADOS
+
+    STR
+    INT
+    FLOAT
+    BOOL
+
+"""
+
+print("Clebson: ", type("Clebson"))
+print(10,type(10))
+print(10.1, type(10.1))
+
+
+
+# boll => converte o item passado em boleano
+print(10 == 10, type(10==10), bool(10==10)) 
+
+# int => converte o item passado em inteiro
+print('10',type('10'), type(int("10")))
+
+# float => converte o item passado em ponto flutuante
+print('10.5',type('10.5'), type(float("10.5")))
+
+
+
