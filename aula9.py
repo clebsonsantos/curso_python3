d61a8d8110e8693a8f55c17607d8813bf049a343
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-04 16:04:30 -0300

    aula9 - condições if, elif, else

---
+++ aula9.py
@@ -0,0 +1,9 @@
+# condições -> id, elif, else
+# condições dentro de hierarquias separadas por espaço
+
+if True: 
+  print("verdadeiro")
+elif False:
+  print("não é verdadeiro")
+else: 
+  print("é verdadeira de novo")
