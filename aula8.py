ce39d3124f42d9a87e27e5ab91e364fdd1ab811a
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-04 15:54:34 -0300

    aula8 - entrada de dados

---
+++ aula8.py
@@ -0,0 +1,7 @@
+# Entrada de dados
+
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+ano_nascimento = 2022 - int(idade)
+
+print(nome, 'tem', idade, 'anos e nasceu em', ano_nascimento)
