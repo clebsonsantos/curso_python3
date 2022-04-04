6d1641fa9b4edf5ce276c8e3cf11166d58e1d62c
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-04 15:10:37 -0300

    aula5 - operadores matematicos

---
+++ aula5.py
@@ -0,0 +1,22 @@
+# Operadores matemáticos
+
+# +
+# -
+# *
+# /
+
+print("adição - ", 10 + 10)
+print("subtração - ", 10 - 10)
+print("multiplicação - ", 10 * 10)
+print("divisão - ", 10 / 10)
+
+
+# // - resultado arredondado da divisão para um numero inteiro
+# ** - exponencial
+# % - resto da divisão entre dois numeros
+# () - operador de precedencia
+
+print("arredondado - ", 10 // 10)
+print("exponencial - ", 10 ** 10)
+print("resto da divisão - ", 10 % 10)
+print("operador de precedencia - ", (10 - 5) + 10)
