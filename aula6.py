8ad773c9d2fa20d0b259d7cb2497dae47d692263
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-04 15:21:28 -0300

    aula6 - variáveis

---
+++ aula6.py
@@ -0,0 +1,18 @@
+# Variáveis:
+
+# Iniciar com letra, pode conter números, separar por _, letras minúsculas
+
+nome = "clebson"
+idade = 26 # int
+altura = 1.79 # float
+maior_de_idade = idade > 18
+peso = 89
+imc = peso / (altura ** 2)
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("É maior: ", maior_de_idade)
+print("Peso: ", peso)
+
+
+print(nome, 'tem', idade, 'de idade e seu imc é', imc)
