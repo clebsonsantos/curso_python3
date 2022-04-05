2dc1bae9693cf09007d2d4abad727f5853b53c6d
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-05 14:46:22 -0300

    aula14 - pass e ellipsis

---
+++ aulas/aula14.py
@@ -0,0 +1,17 @@
+#  pass e ellipsis(...)
+#  Para quando não se pretende implementar nada ainda no código
+
+valor = True
+
+if valor:
+  # usando pass
+  pass
+else:
+  print("erro")
+
+
+if valor:
+  # usando ellipsis
+  ...
+else:
+  print("erro")
