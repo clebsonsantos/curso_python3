5a85723289da7bab9f60a0e08d047f741a67ab1d
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-05 14:17:59 -0300

    aula10- operadores relacionais

---
+++ aula10.py
@@ -0,0 +1,12 @@
+# Operadores relacionais
+
+# == - igual a
+# > - maior que
+# >= - maior ou igual a 
+# <= - menor ou igual a 
+# != - diferente de
+
+num_1 = 2
+num_2 = 2
+expressao = (num_1 == num_2 )
+print(expressao )
