72671bbb3e6aee964a2bc1c533174a1c15a1fc1e
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-16 08:36:38 -0300

    manipulando indices de strings

---
+++ aulas/aula16.py
@@ -0,0 +1,14 @@
+# Manipulando strings 
+# * string indices 
+# * Funções buit-in len, abs, type, print, etc..
+
+texto = 'Python 3'
+# acessando indices positivos da string
+print(texto[2])
+
+text2 = 'Python 2'
+# acessando indices negativos da string
+print(text2[-1])
+# recortando carater
+print(text2[:-1])
+print(text2[-9:-5])
