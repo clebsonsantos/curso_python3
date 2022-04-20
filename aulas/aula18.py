a4a0450df97e0991defb969d5e7745c9a2585735
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-04-20 17:07:52 -0300

    contador e acumulador com while/else

---
+++ aulas/aula18.py
@@ -0,0 +1,16 @@
+# Contadores/acumuladores com while/else
+
+
+contador = 1
+# while contador <= 100:
+#   print(contador)
+#   contador += 1
+
+acumulador = 1
+while contador <= 10:
+  print(contador, acumulador)
+  acumulador = acumulador + contador
+  contador += 1
+else: 
+  print("Acabou")
+
